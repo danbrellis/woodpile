396c64ab2a8d7a59de95cedce4cb611f91e40931
Dan Brellis <danbrellis@gmail.com>
2019-04-11 09:51:11 -0400

    Allows php API server to run simultaneously from api/- moves index.php from public/ to api/; changes php webserver to init in api/ in docker-compose; adds proxy in vue dev server to avoid CORS on API call; groups php routes into api/

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Doctrine\ORM\EntityManager;
@@ -7,52 +8,53 @@ use Woodpile\Entities\Pile;
 use Woodpile\Entities\Stack;
 
 // Routes
+$app->group('/api', function (App $app) {
+    $app->get('/piles', function (Request $request, Response $response, array $args) {
+        /**
+         * @var EntityManager $entityManager
+         */
+        $this->logger->info("Requesting all piles");
 
-$app->get('/piles', function (Request $request, Response $response, array $args) {
-    /**
-     * @var EntityManager $entityManager
-     */
-    $this->logger->info("Requesting all piles");
+        $entityManager = $this->get(EntityManager::class);
+        $pileRepository = $entityManager->getRepository(Pile::class);
+        $piles = $pileRepository->findAll();
 
-    $entityManager = $this->get(EntityManager::class);
-    $pileRepository = $entityManager->getRepository(Pile::class);
-    $piles = $pileRepository->findAll();
+        return $newResponse = $response->withJson($this->serializer->normalize($piles));
+    });
 
-    return $newResponse = $response->withJson($this->serializer->normalize($piles));
-});
+    $app->get('/pile/{pile}', function (Request $request, Response $response, array $args) {
+        /**
+         * @var EntityManager $entityManager
+         */
+        $this->logger->info("Requesting pile with id " . $args['pile']);
 
-$app->get('/pile/{pile}', function (Request $request, Response $response, array $args) {
-    /**
-     * @var EntityManager $entityManager
-     */
-    $this->logger->info("Requesting pile with id ". $args['pile']);
+        $entityManager = $this->get(EntityManager::class);
+        $pileRepository = $entityManager->getRepository(Pile::class);
+        $pile = $pileRepository->findOneBy(['id' => $args['pile']]);
 
-    $entityManager = $this->get(EntityManager::class);
-    $pileRepository = $entityManager->getRepository(Pile::class);
-    $pile = $pileRepository->findOneBy(['id' => $args['pile']]);
+        return $newResponse = $response->withJson($this->serializer->normalize($pile));
+    });
 
-    return $newResponse = $response->withJson($this->serializer->normalize($pile));
-});
+    $app->get('/stack/{stack}', function (Request $request, Response $response, array $args) {
+        $stackId = $args['stack'];
 
-$app->get('/stack/{stack}', function (Request $request, Response $response, array $args) {
-    $stackId = $args['stack'];
+        /**
+         * @var EntityManager $entityManager
+         */
+        $this->logger->info("Requesting stack with id " . $stackId);
 
-    /**
-     * @var EntityManager $entityManager
-     */
-    $this->logger->info("Requesting stack with id ". $stackId);
+        $entityManager = $this->get(EntityManager::class);
+        $stackRepository = $entityManager->getRepository(Stack::class);
+        $stack = $stackRepository->findOneBy(['id' => $stackId]);
 
-    $entityManager = $this->get(EntityManager::class);
-    $stackRepository = $entityManager->getRepository(Stack::class);
-    $stack = $stackRepository->findOneBy(['id' => $stackId]);
+        return $newResponse = $response->withJson($this->serializer->normalize($stack));
+    });
 
-    return $newResponse = $response->withJson($this->serializer->normalize($stack));
-});
+    $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+        // Sample log message
+        $this->logger->info("Slim-Skeleton '/' route");
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+        // Render index view
+        return $this->renderer->render($response, 'index.phtml', $args);
+    });
 });
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:8888",
+        ws: true,
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 };
